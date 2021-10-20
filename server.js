8cf8d2a93d67d3a45a6ac4f1cb5f08022acaced2
derek knox <derekknox14@gmail.com>
2021-10-20 15:52:24 -0700

    adding server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+let PORT;
+const app = express();
+
+if (process.env.JAWSDB_URL) {
+    PORT = process.env.PORT;
+} else {
+    PORT = process.env.PORT || 8080;
+}
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgers_controller');
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+});
