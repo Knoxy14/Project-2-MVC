d68db6beabc49b3bff2e408fcab14428b417e1ee
derek knox <derekknox14@gmail.com>
2021-10-20 16:51:00 -0700

    js and burger image import final

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+const orm = require('../config/orm');
+
+const burger = {
+    all: (callback) => {
+        orm.selectAll('burgers', (res) => {
+            callback(res);
+        });
+    },
+
+    create: (cols, vals, callback) => {
+        orm.insertOne('burgers', cols, vals, (res) => {
+            callback(res)
+        });
+    },
+
+    update: (objColVals, condition, callback) => {
+        orm.updateOne('burgers', objColVals, condition, (res) => {
+            callback(res);
+        })
+    },
+
+    delete: (condition, callback) => {
+        orm.delete('burgers', condition, (res) => {
+            callback(res);
+        })
+    }
+}
+
+module.exports = burger;
